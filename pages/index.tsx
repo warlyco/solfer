b956f40489d0d57c35b580fdef2c0f8e7c390c27
Dan Ward <danward@gmail.com>
2023-02-07 02:54:39 -0800

    Restore upload component

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,8 @@ export default function Home() {
     <>
       <main className="w-full min-h-screen flex items-center justify-center">
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        {/* <ShadowUpload /> */}
-        <LitEncrypt />
+        <ShadowUpload />
+        {/* <LitEncrypt /> */}
       </main>
     </>
   );
